c35423d1bd783b0220e2c277112cb4a568c008de
Captain Shadows <captain_shadows@aol.com>
2013-05-26 21:17:39 -0400

    The beging of Models

---
+++ common/ccm/harvestry/client/model/ModelGrill_Old.java
@@ -0,0 +1,32 @@
+package ccm.harvestry.client.model;
+
+import net.minecraftforge.client.model.AdvancedModelLoader;
+import net.minecraftforge.client.model.IModelCustom;
+import ccm.harvestry.utils.lib.Models;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ModelGrill_Old
+{
+
+    private final IModelCustom modelGrill;
+
+    public ModelGrill_Old()
+    {
+
+        this.modelGrill = AdvancedModelLoader.loadModel(Models.GRILL);
+    }
+
+    public void render()
+    {
+
+        this.modelGrill.renderAll();
+    }
+
+    public void renderPart(final String partName)
+    {
+
+        this.modelGrill.renderPart(partName);
+    }
+}
