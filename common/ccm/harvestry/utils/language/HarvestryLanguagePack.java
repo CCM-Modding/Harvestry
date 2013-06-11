1fed045fa7074e1dfab61f47edbb478e193b49a8
Captain Shadows <captain_shadows@aol.com>
2013-06-11 16:15:36 -0400

    stuff 2

--- common/ccm/harvestry/utils/language/HarvestryLanguagePack.java
+++ common/ccm/harvestry/utils/language/HarvestryLanguagePack.java
@@ -1,5 +1,8 @@
 package ccm.harvestry.utils.language;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 import ccm.nucleum_omnium.helper.LanguageHelper;
@@ -11,19 +14,24 @@ public class HarvestryLanguagePack extends ILanguagePack
 {
 
     /**
+     * Private {@code List<String>} that contains the languages, Shared with all Language Packs
+     */
+    private final List<String> supportedLanguages = new ArrayList<String>();
+
+    /**
      * Adds all the supported Languages
      */
     public HarvestryLanguagePack()
     {
         super(Locations.LANGUAGE_FILE);
-        supportedLanguages.add(getPath("en_US"));
-        supportedLanguages.add(getPath("es_ES"));
+        this.supportedLanguages.add(this.getPath("en_US"));
+        this.supportedLanguages.add(this.getPath("es_ES"));
     }
 
     @Override
     public void loadLangs()
     {
-        for (String langFile : supportedLanguages){
+        for (final String langFile : this.supportedLanguages){
             LanguageRegistry.instance().loadLocalization(langFile, LanguageHelper.getLangFromFileName(langFile), LanguageHelper.isXMLLanguageFile(langFile));
         }
     }
